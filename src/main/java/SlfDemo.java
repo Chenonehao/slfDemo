2ac636f4fb4f78f0644652fcd8d87ea7d640945a
ChenYiHao <a820158668@163.com>
2019-10-22 16:07:28 +0800

    first commit

---
+++ src/main/java/SlfDemo.java
@@ -0,0 +1,33 @@
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SlfDemo {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SlfDemo.class);
+
+    public static void main(String[] args) {
+        // 获取classpath路径
+        String s = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        System.out.println("classpath => " + s );
+
+        // 获取classpath路径
+        String path = SlfDemo.class.getResource("/").toString();
+        System.out.println("classpath => " + path);
+
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        StatusPrinter.print(lc);
+        System.out.println();
+        test();
+    }
+
+    public static void test() {
+        LOGGER.trace("trace level");
+        LOGGER.debug("debug level");
+        LOGGER.info("info level");
+        LOGGER.error("error level");
+
+    }
+
+}
